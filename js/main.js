9688b1242b61011d7f97f3cd24abf3f0797b5f11
mario fernando <mariofdo21hernandezvilla@gmail.com>
2022-10-04 22:20:27 -0500

    mdificaciones iniciales mario f hernandez

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function changeColor(newColor) {
+   // const elem = document.getElementById('para');
+   const elem1 = document.getElementById('titulo1');
+   const elem2 = document.getElementById('titulo2');
+    elem1.style.color = newColor;
+    elem2.style.color = newColor;
+  }
+  
+let firstHeading = document.querySelector('h1');
+
+console.log(firstHeading);
